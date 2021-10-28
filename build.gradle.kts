eb7bd9f4bf9821bc62d8f593f6e78c2fa4941afc
LovesAsuna <qq625924077@gmail.com>
2021-10-29 00:06:55 +0800

    :arrow_up: build(gradle): upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,14 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
 }
 
 //工具
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.20")
-    annotationProcessor("org.projectlombok:lombok:1.18.20")
+    implementation("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
     testImplementation("org.junit.jupiter:junit-jupiter:5.830")
     implementation("info.picocli:picocli:4.6.1")
 }
