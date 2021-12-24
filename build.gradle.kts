a0fa02502276cba946691ea7e1609de8b4ebb615
LovesAsuna <qq625924077@gmail.com>
2021-12-24 09:22:25 +0800

    :arrow_up: build(gradle): upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    kotlin("jvm") version "1.6.10"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
@@ -12,20 +13,20 @@ repositories {
 
 // 日志
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.16.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.16.0")
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
 }
 
-//工具
+// 工具
 dependencies {
     implementation("org.projectlombok:lombok:1.18.22")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     testImplementation("org.junit.jupiter:junit-jupiter:5.830")
-    implementation("info.picocli:picocli:4.6.1")
+    implementation("info.picocli:picocli:4.6.2")
 }
 
 tasks.jar {
